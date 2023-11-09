eaa6a85bec31ae5c7944a5104d61b060797b0f21
xxf185 <109097973+xxf185@users.noreply.github.com>
2023-11-09 09:48:40 +0800

    Update sing-REALITY-box.sh

--- sing-REALITY-box.sh
+++ sing-REALITY-box.sh
@@ -12,9 +12,9 @@ print_with_delay() {
 }
 
 # Introduction animation
-echo ""
-echo "sing-REALITY-box 一键安装脚本" 
-echo ""
+echo "#############################"
+echo "#sing-REALITY-box 一键安装脚本#" 
+echo "#############################"
 echo ""
 
 
