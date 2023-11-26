f429c22b53c11ff8d448c9f157bc66e1974c15a7
xxf185 <109097973+xxf185@users.noreply.github.com>
2023-11-26 10:19:03 +0800

    Update sing-REALITY-box.sh

--- sing-REALITY-box.sh
+++ sing-REALITY-box.sh
@@ -236,8 +236,8 @@ read -p "输入listen port (默认: 443): " listen_port
 listen_port=${listen_port:-443}
 echo ""
 # Ask for server name (sni)
-read -p "输入server name/SNI (默认: one-piece.com): " server_name
-server_name=${server_name:-one-piece.com}
+read -p "输入server name/SNI (默认: pluto.tv): " server_name
+server_name=${server_name:pluto.tv}
 
 # Retrieve the server IP address
 server_ip=$(curl -s https://api.ipify.org)
