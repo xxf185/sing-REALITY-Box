7a7276a1172d2505aa3c2002328c2617bd873e3d
xxf185 <109097973+xxf185@users.noreply.github.com>
2024-06-06 21:56:19 +0800

    更新 sing-REALITY-box.sh

--- sing-REALITY-box.sh
+++ sing-REALITY-box.sh
@@ -236,8 +236,8 @@ read -p "输入listen port (默认: 443): " listen_port
 listen_port=${listen_port:-443}
 echo ""
 # Ask for server name (sni)
-read -p "输入server name/SNI (default: one-piece.com): " server_name
-server_name=${server_name:-one-piece.com}
+read -p "输入server name/SNI (default: dash.cloudflare.com): " server_name
+server_name=${server_name:-dash.cloudflare.com}
 
 # Retrieve the server IP address
 server_ip=$(curl -s https://api.ipify.org)
